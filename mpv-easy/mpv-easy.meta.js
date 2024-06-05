824f2f92f0b637d2b6ce421d0dd5085333c830e2
ahaoboy <504595380@qq.com>
2024-06-05 18:36:24 +0800

    ci: add meta file

---
+++ mpv-easy/mpv-easy.meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         mpv-easy
+// @version      0.1.9-alpha.1
+// @description  An easy-to-use UI implemented with js
+// @author       mpv-easy
+// @downloadURL  https://github.com/mpv-easy/mpv-easy/releases/0.1.9-alpha.1/download/mpv-easy-es5.js
+// ==/UserScript==
