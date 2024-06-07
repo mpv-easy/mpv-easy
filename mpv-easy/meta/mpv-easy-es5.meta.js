3701fa680b318fa5d305de36718103b042da209e
ahaoboy <504595380@qq.com>
2024-06-07 21:43:13 +0800

    v0.1.9

--- mpv-easy/meta/mpv-easy-es5.meta.js
+++ mpv-easy/meta/mpv-easy-es5.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mpv-easy-es5
-// @version      0.1.9-alpha.3
+// @version      0.1.9
 // @description  An easy-to-use UI implemented with js
 // @author       mpv-easy
 // @downloadURL  https://github.com/mpv-easy/mpv-easy/releases/latest/download/mpv-easy-es5.js
