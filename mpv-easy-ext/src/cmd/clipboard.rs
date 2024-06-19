9a5f0a815bf66fb6b4ddb89a185f2d2a80cdffc3
ahaoboy <504595380@qq.com>
2024-06-19 22:20:37 +0800

    ext: add clipboard-rs for android

--- mpv-easy-ext/src/cmd/clipboard.rs
+++ mpv-easy-ext/src/cmd/clipboard.rs
@@ -8,22 +8,39 @@ pub struct Clipboard {
     #[clap(required = false, default_value_t = String::new())]
     text: String,
 }
-use clipboard_rs::{common::RustImage, Clipboard as _, ClipboardContext};
 
-pub fn set_text(text: &str) {
-    let ctx = ClipboardContext::new().unwrap();
-    ctx.set_text(text.to_string()).unwrap();
+#[cfg(not(android))]
+mod clip {
+    use clipboard_rs::{common::RustImage, Clipboard as _, ClipboardContext};
+    pub fn set_text(text: &str) {
+        let ctx = ClipboardContext::new().unwrap();
+        ctx.set_text(text.to_string()).unwrap();
+    }
+
+    pub fn get_text() -> String {
+        let ctx = ClipboardContext::new().unwrap();
+        ctx.get_text().unwrap()
+    }
+
+    pub fn set_image(path: &str) {
+        let ctx = ClipboardContext::new().unwrap();
+        let img = RustImage::from_path(path).unwrap();
+        ctx.set_image(img).unwrap();
+    }
 }
+#[cfg(android)]
+mod clip {
+    pub fn set_text(text: &str) {
+        todo!()
+    }
 
-pub fn get_text() -> String {
-    let ctx = ClipboardContext::new().unwrap();
-    ctx.get_text().unwrap()
-}
+    pub fn get_text() -> String {
+        todo!()
+    }
 
-pub fn set_image(path: &str) {
-    let ctx = ClipboardContext::new().unwrap();
-    let img = RustImage::from_path(path).unwrap();
-    ctx.set_image(img).unwrap();
+    pub fn set_image(path: &str) {
+        todo!()
+    }
 }
 
 pub fn get_image() {
@@ -41,15 +58,15 @@ impl Cmd for Clipboard {
                 let b64: String = serde_json::from_str(self.text.as_str()).unwrap();
                 let bin = BASE64_STANDARD.decode(b64).unwrap();
                 let text = String::from_utf8_lossy(&bin);
-                set_text(&text)
+                clip::set_text(&text)
             }
             "get" => {
-                let s = get_text();
+                let s = clip::get_text();
                 println!("{}", serde_json::to_string(&s).unwrap());
             }
             "set-image" => {
                 let text = serde_json::from_str(self.text.as_str()).unwrap();
-                set_image(text);
+                clip::set_image(text);
             }
             "get-image" => {
                 get_image();
