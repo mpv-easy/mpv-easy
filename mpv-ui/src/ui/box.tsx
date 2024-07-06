659486abe4a9a44931b7eee2ab85686610956025
ahaoboy <504595380@qq.com>
2024-07-06 12:23:32 +0800

    fix ci

--- mpv-ui/src/ui/box.tsx
+++ mpv-ui/src/ui/box.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { forwardRef } from "react"
-import type { MpDom } from "../render/dom"
+import type { MpDom, MpDomProps } from "../render/dom"
 
 export type Ref = MpDom
 
 export const Box = forwardRef<
   Ref,
-  Partial<MpDom["attributes"] & { children?: React.ReactNode }>
+  Partial<MpDomProps & { children: React.ReactNode }>
 >((props, ref) => {
   // @ts-ignore
   return props.display !== "none" && <mpv-box ref={ref} {...props} />
