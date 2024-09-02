ccef827f0193cb59fbc20509b61dad99e24e9af8
ahaoboy <504595380@qq.com>
2024-09-02 21:59:21 +0800

    easy: bump dep

--- mpv-easy-react/src/example/counter-jotai.tsx
+++ mpv-easy-react/src/example/counter-jotai.tsx
@@ -1,32 +1,32 @@
-import React, { useEffect } from "react";
-import { Provider, atom, useAtom } from "jotai";
-import { Box, render } from "@mpv-easy/react";
+import React, { useEffect } from "react"
+import { Provider, atom, useAtom } from "jotai"
+import { Box, render } from "@mpv-easy/react"
 
-const countAtom = atom(0);
-const doubleAtom = atom((get) => get(countAtom) * 2);
-const ltTenAtom = atom((get) => get(countAtom) < 10);
+const countAtom = atom(0)
+const doubleAtom = atom((get) => get(countAtom) * 2)
+const ltTenAtom = atom((get) => get(countAtom) < 10)
 
 function Counter() {
-  const [count, setCount] = useAtom(countAtom);
+  const [count, setCount] = useAtom(countAtom)
   useEffect(() => {
     setInterval(() => {
-      setCount((v) => v + 1);
-    }, 1000);
-  }, []);
-  console.log("count", count);
-  return <Box text={count.toString()} />;
+      setCount((v) => v + 1)
+    }, 1000)
+  }, [])
+  console.log("count", count)
+  return <Box text={count.toString()} />
 }
 
 function Double() {
-  const [double] = useAtom(doubleAtom);
-  console.log("double", double);
-  return <Box text={double.toString()} />;
+  const [double] = useAtom(doubleAtom)
+  console.log("double", double)
+  return <Box text={double.toString()} />
 }
 
 function LtTen() {
-  const [ltTen] = useAtom(ltTenAtom);
-  console.log("ltTen", ltTen);
-  return <Box text={ltTen.toString()} />;
+  const [ltTen] = useAtom(ltTenAtom)
+  console.log("ltTen", ltTen)
+  return <Box text={ltTen.toString()} />
 }
 
 const App = () => (
@@ -35,6 +35,6 @@ const App = () => (
     <Double />
     <LtTen />
   </Provider>
-);
+)
 
-render(<App />);
+render(<App />)
