b5bfcfe2c0a8269be1ba79859206d6036d062f03
ahaoboy <504595380@qq.com>
2024-09-02 21:18:56 +0800

    easy: add mobx and jotai example

---
+++ mpv-easy-react/src/example/counter-jotai.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import { Provider, atom, useAtom } from "jotai";
+import { Box, render } from "@mpv-easy/react";
+
+const countAtom = atom(0);
+const doubleAtom = atom((get) => get(countAtom) * 2);
+const ltTenAtom = atom((get) => get(countAtom) < 10);
+
+function Counter() {
+  const [count, setCount] = useAtom(countAtom);
+  useEffect(() => {
+    setInterval(() => {
+      setCount((v) => v + 1);
+    }, 1000);
+  }, []);
+  console.log("count", count);
+  return <Box text={count.toString()} />;
+}
+
+function Double() {
+  const [double] = useAtom(doubleAtom);
+  console.log("double", double);
+  return <Box text={double.toString()} />;
+}
+
+function LtTen() {
+  const [ltTen] = useAtom(ltTenAtom);
+  console.log("ltTen", ltTen);
+  return <Box text={ltTen.toString()} />;
+}
+
+const App = () => (
+  <Provider>
+    <Counter />
+    <Double />
+    <LtTen />
+  </Provider>
+);
+
+render(<App />);
---
+++ mpv-easy-react/src/example/counter-mobx.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { makeAutoObservable } from "mobx";
+import { observer } from "mobx-react-lite";
+import { Box, render } from "@mpv-easy/react";
+
+function createState() {
+  return makeAutoObservable({
+    count: 0,
+    increase() {
+      this.count += 1;
+    },
+    reset() {
+      this.count = 0;
+    },
+  });
+}
+
+const state = createState();
+type State = ReturnType<typeof createState>;
+const App = observer(({ state }: { state: State }) => (
+  <Box onClick={() => state.increase()} text={state.count.toString()} />
+));
+
+render(<App state={state} />);
+
+setInterval(() => {
+  state.increase();
+}, 1000);
