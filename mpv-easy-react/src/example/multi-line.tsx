f3069944217df419a35da9219c212bdda03db931
ahaoboy <504595380@qq.com>
2024-10-02 13:06:35 +0800

    example: add multi-line

---
+++ mpv-easy-react/src/example/multi-line.tsx
@@ -0,0 +1,26 @@
+import { Box } from "@mpv-easy/react"
+import React from "react"
+
+export default function MultiLine() {
+  return (
+    <>
+      <Box
+        id="box-main"
+        width={"50%"}
+        height={"50%"}
+        display="flex"
+        backgroundColor="#000000A0"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box
+          text={"aaa\nbbb"}
+          color="#FF0000"
+          backgroundColor="#00FFFF10"
+          width={200}
+          height={200}
+        />
+      </Box>
+    </>
+  )
+}
