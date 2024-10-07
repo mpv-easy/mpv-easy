55fe98efae78f91c0e42b973f648b768584d92a9
ahaoboy <504595380@qq.com>
2024-10-07 17:26:02 +0800

    translate: add build.ts

---
+++ mpv-translate/build.ts
@@ -0,0 +1,19 @@
+import { build } from "esbuild"
+import fs from "node:fs"
+
+build({
+  entryPoints: ["./src/mpv-translate.tsx"],
+  bundle: true,
+  outdir: "bundle",
+  minify: true,
+
+  charset: "utf8",
+  define: {
+    "process.env.NODE_ENV": '"production"',
+  },
+  metafile: true,
+})
+  .then((r) => {
+    fs.writeFileSync("./bundle/metafile.json", JSON.stringify(r.metafile))
+  })
+  .catch(() => process.exit(1))
