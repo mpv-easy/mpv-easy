ccc38ec9736a32a51c3211ea49515e0c32a826fd
ahaoboy <504595380@qq.com>
2024-12-25 18:29:01 +0800

    remove unused file

--- mpv-easy-react/meta/mpv-easy-es5.meta.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name         mpv-easy
-// @version      0.1.9
-// @description  An easy-to-use UI implemented with js
-// @author       mpv-easy
-// @downloadURL  https://github.com/mpv-easy/mpv-easy/releases/latest/download/mpv-easy.js
-// ==/UserScript==
