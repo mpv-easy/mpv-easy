bec1381a2b92e4f34c7026a15edf92576e077ec5
ahaoboy <504595380@qq.com>
2025-03-03 09:49:51 +0800

    logo add click

--- mpv-easy-react/src/ui/components/logo.tsx
+++ mpv-easy-react/src/ui/components/logo.tsx
@@ -1,11 +1,24 @@
-import React from "react"
+import React, { useRef } from "react"
 import { Box } from "@mpv-easy/react"
 import * as ICON from "../../icon"
-import { iconButtonStyle, logoStyle, useSelector } from "../../store"
+import { dispatch, iconButtonStyle, logoStyle, useSelector } from "../../store"
+import { openDialog } from "@mpv-easy/tool"
 const TEXT = "Drop files or URLs to play here."
 export const Logo = () => {
   const style = useSelector(logoStyle)
   const buttonStyle = useSelector(iconButtonStyle)
+  const selecting = useRef(false)
+  const select = () => {
+    if (selecting.current) {
+      return
+    }
+    selecting.current = true
+    const list = openDialog()
+    if (list.length) {
+      dispatch.setPlaylist(list, 0)
+    }
+    selecting.current = false
+  }
   return (
     <Box
       display="flex"
@@ -16,12 +29,13 @@ export const Logo = () => {
       alignItems="center"
       flexDirection="row"
     >
-      <Box text={ICON.Play} {...style} />
+      <Box text={ICON.Play} {...style} onClick={select} />
       <Box
         text={TEXT}
         font={buttonStyle.font}
         color={buttonStyle.color}
         fontSize={buttonStyle.fontSize}
+        onClick={select}
       />
     </Box>
   )
