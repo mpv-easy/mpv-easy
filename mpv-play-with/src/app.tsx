bde2b91723614f78796ef1066c8742c6ce4c7fca
ahaoboy <504595380@qq.com>
2025-04-11 16:52:29 +0800

    play-with: right click to lock icon

--- mpv-play-with/src/app.tsx
+++ mpv-play-with/src/app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState, WheelEvent } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Bilibili, Jellyfin, Youtube, Twitch, Nicovideo } from "./rules"
 import {
   encodeToBase64,
