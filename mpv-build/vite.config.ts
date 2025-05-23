d7542a96ecd975b8b72afb9d75df973db95388a5
ahaoboy <504595380@qq.com>
2025-05-23 23:04:52 +0800

    mpv-build: base

--- mpv-build/vite.config.ts
+++ mpv-build/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/mpv-build/",
+  base: "https://mpv-easy.github.io/mpv-build/",
   plugins: [react()],
 })
