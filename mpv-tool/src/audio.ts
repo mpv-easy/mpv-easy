e90324506857171f7bd3ffa76a33051d12d35fdd
ahaoboy <504595380@qq.com>
2025-08-09 23:20:38 +0800

    playAudio: add quote

--- mpv-tool/src/audio.ts
+++ mpv-tool/src/audio.ts
@@ -4,6 +4,6 @@ import { getMpvExePath } from "./mpv"
 export function playAudio(url: string, volume = 100, loop = 0) {
   const mpv = getMpvExePath()
   return runCmdAsync(
-    `${mpv} --load-scripts=no --loop=${loop} --volume=${volume} --force-window=no ${url}`,
+    `${mpv} --load-scripts=no --loop=${loop} --volume=${volume} --force-window=no "${url}"`,
   )
 }
