7425a7a0131dbf6995cfcf215c75428f79c97077
ahaoboy <504595380@qq.com>
2025-08-22 19:51:47 +0800

    build: analyzer

--- mpv-build/vite.config.ts
+++ mpv-build/vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import analyzer from "vite-bundle-analyzer"
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "https://mpv-easy.github.io/mpv-build/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    analyzer({
+      enabled: !!process.env.analyzer,
+    }),
+  ],
+  resolve: {
+    alias: {
+      react: "https://esm.sh/react",
+      "react-dom": "https://esm.sh/react-dom",
+      "@easy-install/easy-archive": "https://esm.sh/@easy-install/easy-archive",
+    },
+  },
 })
