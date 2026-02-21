2369a128f84a6b029ee3c93066d6c3258be29e72
ahaoboy <504595380@qq.com>
2026-02-21 16:39:21 +0800

    overlay: add getOverlayId

--- mpv-tool/src/overlay.ts
+++ mpv-tool/src/overlay.ts
@@ -3,6 +3,33 @@ import { overlayAdd, overlayRemove } from "./type-cmd"
 const maxId = 64
 const overlayIdUsed = new Array(maxId).map(() => false)
 
+/**
+ * Get an available overlay ID.
+ * https://mpv.io/manual/stable/#command-interface-overlay-add
+ * @param preferredId The ID you'd like to use.
+ * @returns An available ID between 0 and 63.
+ * @throws Error if no ID is available.
+ */
+function getOverlayId(preferredId: number): number {
+  if (preferredId < 0 || preferredId >= maxId) {
+    throw new Error(`overlay's id(${preferredId}) must be in the range [0, 63]`)
+  }
+
+  if (preferredId >= 0 && preferredId < maxId && !overlayIdUsed[preferredId]) {
+    overlayIdUsed[preferredId] = true
+    return preferredId
+  }
+
+  for (let i = 0; i < maxId; i++) {
+    if (!overlayIdUsed[i]) {
+      overlayIdUsed[i] = true
+      return i
+    }
+  }
+
+  throw new Error("No available overlay ID (0-63)")
+}
+
 export class Overlay {
   public x = 0
   public y = 0
@@ -14,15 +41,16 @@ export class Overlay {
   public stride = 0
   public dw: number | undefined = undefined
   public dh: number | undefined = undefined
+  public id: number
 
-  constructor(public id: number) {
-    if (overlayIdUsed[id]) {
-      throw new Error(`overlay's id has already been used.${id}`)
+  constructor(id: number) {
+    const finalId = getOverlayId(id)
+    if (finalId !== id) {
+      print(
+        `[overlay] ID ${id} is already in use, assigned ${finalId} instead.`,
+      )
     }
-    if (id < 0 || id >= maxId) {
-      throw new Error(`overlay's id must be in the range [0, 63]${id}`)
-    }
-    overlayIdUsed[id] = true
+    this.id = finalId
   }
 
   update() {
