58b699f94854d84667893a8d1ae65330f28bd405
ahaoboy <504595380@qq.com>
2026-02-24 20:15:27 +0800

    tool: uosc-version

--- mpv-easy-react/scripts/mpv-easy.tsx
+++ mpv-easy-react/scripts/mpv-easy.tsx
@@ -1,21 +1,16 @@
 import "@mpv-easy/polyfill"
-import { runMpvEasy } from "../src/mpv-easy"
-import { DefaultFps } from "@mpv-easy/react"
-import { getenv } from "@mpv-easy/tool"
+import { uoscVersion } from "@mpv-easy/tool"
 
-const SHOW_FPS = !!getenv("SHOW_FPS")
-const MAX_FPS_FRAME = getenv("MAX_FPS_FRAME")
-const FRAME_LIMIT = getenv("FRAME_LIMIT")
-runMpvEasy({
-  fps: DefaultFps,
-  showFps: SHOW_FPS,
-  maxFpsFrame: MAX_FPS_FRAME ? +MAX_FPS_FRAME : 64,
-  frameLimit: FRAME_LIMIT ? +FRAME_LIMIT : 0,
-})
-// import React from "react"
-// import { ColorBoxDirty } from "./example/color-box-dirty"
-// import { createRender } from "@mpv-easy/react"
+console.log(
+  "uoscVersion",
+  uoscVersion().then((i) => {
+    console.log("iiii---", i)
+  }),
+)
 
-// createRender({
-//   showFps: true,
-// })(<ColorBoxDirty />)
+async function r() {
+  console.log("v---1", await uoscVersion())
+  console.log("v---2", await uoscVersion())
+}
+
+r()
--- mpv-tool/src/uosc.ts
+++ mpv-tool/src/uosc.ts
@@ -1,11 +1,20 @@
 import { registerScriptMessage } from "./mpv"
+let UOSC_VERSION: string | undefined
+const UOSC_VERSION_P: Promise<string | undefined> = new Promise((r) => {
+  if (mp) {
+    registerScriptMessage("uosc-version", (v) => {
+      r(v)
+    })
 
-const UOSC_VERSION: Promise<string | undefined> = new Promise((r) => {
-  registerScriptMessage("uosc-version", (v) => {
-    r(v)
-  })
+    setTimeout(() => {
+      r(UOSC_VERSION)
+    }, 100)
+  }
 })
 
 export function uoscVersion() {
-  return UOSC_VERSION
+  if (UOSC_VERSION) {
+    return UOSC_VERSION
+  }
+  return UOSC_VERSION_P
 }
