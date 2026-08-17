1fac0e4c7c4a8475b3b24a43d70447e8442c0191
ahaoboy <504595380@qq.com>
2026-08-17 20:22:36 +0800

    bump

--- mpv-easy-react/src/example/rematch/models/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Models } from "@rematch/core"
-import { counter } from "./counter"
-
-export interface RootModel extends Models<RootModel> {
-  counter: typeof counter
-}
-
-export const models: RootModel = { counter }
